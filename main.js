e492bddb29656f5b343412f69d4298bf0818d681
nol3 <alejandrocp_952@hotmail.com>
2020-09-07 19:43:48 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
-  document.querySelector('.nav-menu').classList.toggle('show')
-});
-
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.cartas-luna', { delay: 300});
-ScrollReveal().reveal('.cartas-banner-uno', { delay: 400});
-ScrollReveal().reveal('.cartas-banner-dos', { delay: 500});
